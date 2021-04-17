e7d43db3415776c490f68bfba74debbc4db8f28d
Jeferson Grueso <jefersong@titamedia.com>
2021-04-17 12:16:42 -0500

    sass iny slider and basic template
    	Jeferson Grueso

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+
+
+const slider = () => {
+    var slider = tns({
+        container: '.slider',
+        items: 1,
+        slideBy: 'page',
+        autoplay: true
+      });}
+
+
+      slider();
