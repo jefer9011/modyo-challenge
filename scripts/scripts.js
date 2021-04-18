2484e2f0b6216f6a5da5fb0f5b2f4d384cd56312
Jeferson Grueso <jefersong@titamedia.com>
2021-04-17 22:38:26 -0500

    Landing maquetada completa
    	Jeferson Grueso

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 
 
 const slider = () => {
-    var slider = tns({
+    let slider = tns({
         container: '.slider',
         items: 1,
         slideBy: 'page',
@@ -11,10 +11,24 @@ const slider = () => {
         autoplayTimeout: 20000,
         autoplayButton: false,
         loop: true,
- 
-
-
       });}
 
+      const sliderTestimonials = () => {
+        let slider2 = tns({
+            container: '.testimonials__slide',
+            items: 1,
+            slideBy: 'page',
+            autoplay: true,
+            gutter	: '20px',
+            speed: 700,
+            autoplayTimeout: 20000,
+            autoplayButton: false,
+            loop: true,
+          });}
 
-      slider();
+      let bodyHome = document.querySelector('.home');
+      if(bodyHome) {
+          console.log('estoy en home');
+          slider();
+          sliderTestimonials();
+      }
