e45627835e120e42a2994b632bccd8b27503350a
Jeferson Grueso <jefersong@titamedia.com>
2021-04-17 19:36:09 -0500

    Ajustes landing

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,7 +5,15 @@ const slider = () => {
         container: '.slider',
         items: 1,
         slideBy: 'page',
-        autoplay: true
+        autoplay: true,
+        gutter	: '20px',
+        speed: 700,
+        autoplayTimeout: 20000,
+        autoplayButton: false,
+        loop: true,
+ 
+
+
       });}
 
 
